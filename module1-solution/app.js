5539ef43c13e8812c33dd91fc2c15dc87162495a
Oksana Koyfman <koyfo@mail.broward.edu>
2016-09-18 17:28:21 -0400

    Added color to font and border

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -6,16 +6,23 @@
   function lunchCheckController($scope){
     $scope.message = "";
     $scope.listOfItems = "";
+    $scope.borderSize = "0";
     $scope.checkLunch = function(){
       var listOfItems = $scope.listOfItems.split(',');
       if($scope.listOfItems == ""){
         $scope.message = "Please enter data first";
+        $scope.color = "red";
+        $scope.borderSize = "1";
       }
       else if(listOfItems.length > 0 && listOfItems.length < 4){
         $scope.message = "Enjoy!";
+        $scope.color = "green";
+        $scope.borderSize = "1";
       }
       else {
         $scope.message = "Too much!";
+        $scope.color = "green";
+        $scope.borderSize = "1";
       }
     };
   }
